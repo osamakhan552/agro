d9b15f74b7ee968991b23979b349f17e33846813
osama <usamak552@gmail.com>
2021-05-21 20:17:55 +0530

    added row

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('AGRO'),
+          title: Text('AGRO APP'),
         ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget> [
+          RaisedButton(
+            onPressed: (){},
+            child: Icon(
+              Icons.volume_off,
+              color: Colors.black,
+            ),
+            color: Colors.grey,
+          ),
+
+        ],
+      ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print('Osama');
+          },
+          backgroundColor: Colors.red[500],
+          child: Text('Press',
+          ),
+          splashColor: Colors.black,
+        ),
+
+
       ),
     );
   }
